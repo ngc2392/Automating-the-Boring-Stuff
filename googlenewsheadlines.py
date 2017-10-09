93f68b2f2c38bd7e2893a060e78d6fb4bc133090
Logan Phillips <LoganPhillips@Logans-MacBook-Pro-2.local>
2017-10-09 15:45:15 -0400

    summarizes articles, but gives an error eventually

--- googlenewsheadlines.py
+++ googlenewsheadlines.py
@@ -3,12 +3,23 @@
 #Get news titles from google news, output a summary
 import requests
 from bs4 import BeautifulSoup
+from sumy.parsers.html import HtmlParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
+
+#https://github.com/miso-belica/sumy
 #this gets more than just title.  Figure out how to get just title
 url='https://news.google.com/news/headlines?hl=en'
 webpageText = requests.get(url)
 soup = BeautifulSoup(webpageText.text, 'html.parser')
 #print(soup.prettify())
 headlines = soup.find_all('a',role="heading")
+
+LANGUAGE = "english"
+SENTENCES_COUNT = 10
+
 for headlineName in headlines:
     articleLink = headlineName["href"]
     #print("ALL HEADLINE: " + str(headlineName))
@@ -20,6 +31,13 @@ for headlineName in headlines:
     if(headlineName["aria-level"] is "2"): #ignore 'RELATED COVERAGE'
         print("HEADLINE: " + headlineName.text)
         print("LINK OF ABOVE HEADLINE: " + articleLink)
+        tempUrl = articleLink
+        parser = HtmlParser.from_url(tempUrl, Tokenizer(LANGUAGE))
+        stemmer = Stemmer(LANGUAGE)
+        summarizer = Summarizer(stemmer)
+        summarizer.stop_words = get_stop_words(LANGUAGE)
+        for sentence in summarizer(parser.document, SENTENCES_COUNT):
+            print(sentence)
     
         print(" ")
         

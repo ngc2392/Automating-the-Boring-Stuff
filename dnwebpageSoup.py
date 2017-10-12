d8cd9201148429c77e52f7ff82d6393da45b21ce
Logan Phillips <LoganPhillips@logans-mbp-2.lvc.edu>
2017-10-11 23:38:21 -0400

    python script that uses Beautiful Soup 4 to scrape web page.  Will allow command line arguments later

--- dnwebpageSoup.py
+++ dnwebpageSoup.py
@@ -1,1 +1,17 @@
+#! /Library/Frameworks/Python.framework/Versions/3.5/bin/python3.5
+
 #this will get romeo and juliet using beatiful soup
+
+import requests
+from bs4 import BeautifulSoup
+from sumy.parsers.html import HtmlParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
+
+
+url='http://www.gutenberg.org/cache/epub/1112/pg1112.txt'
+webpageText = requests.get(url)
+soup = BeautifulSoup(webpageText.text, 'html.parser')
+print(soup.prettify())
